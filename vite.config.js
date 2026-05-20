631631f271b03867e4abc56f8f6e01da99e9c1b8
Zee <asvinandika19@gmail.com>
2026-05-20 23:25:30 +0800

    i

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,22 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), 
+//     tailwindcss()
+//   ],
+// })
+
 export default defineConfig({
-  plugins: [react(), 
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          animation: ['framer-motion'],
+        }
+      }
+    }
+  }
 })
